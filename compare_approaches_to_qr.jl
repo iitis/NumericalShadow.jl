de38e3491f03efa1d8690041970d99fa474dd46e
Łukasz Pawela <lukasz.pawela@gmail.com>
2023-06-28 03:08:27 +0200

    compare approaches to batch calculations

--- compare_approaches_to_qr.jl
+++ compare_approaches_to_qr.jl
@@ -2,3 +2,31 @@ using CUDA
 
 # compare gelsBatched, geqrfBatched and CPU generation
 # the GPU routines will probably require to use inv and multiply the inpu by R^-1 to obtain Q (getri, getrf)
+
+function non_strided(a, b)
+    x = [@view a[:, :, i] for i in axes(a, 3)]
+    y = [@view b[:, :, i] for i in axes(b, 3)]
+    CUDA.CUBLAS.gemm_batched('N', 'N', 1f0, x, y)
+end
+
+function strided(a, b)
+    CUDA.CUBLAS.gemm_strided_batched('N', 'N', 1f0, a, b)
+end
+
+function generate_matrices(d, nbatch)
+    a = CUDA.rand(d, d, nbatch)
+    b = CUDA.rand(d, d, nbatch)
+    a, b
+end
+
+a, b = generate_matrices(10, 10)
+non_strided(a, b)
+strided(a, b)
+
+a, b = generate_matrices(4, 10^7)
+CUDA.@time CUDA.@sync non_strided(a, b)
+CUDA.@time CUDA.@sync strided(a, b)
+
+a, b = generate_matrices(1000, 32)
+CUDA.@time CUDA.@sync non_strided(a, b)
+CUDA.@time CUDA.@sync strided(a, b)
