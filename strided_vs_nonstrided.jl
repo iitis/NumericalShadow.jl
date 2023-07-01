83dcfebd98ee1e9869c97833ecb0070662617573
Łukasz Pawela <lukasz.pawela@gmail.com>
2023-07-01 02:04:25 +0200

    rename file to comparison

--- strided_vs_nonstrided.jl
+++ strided_vs_nonstrided.jl
@@ -4,8 +4,10 @@ using CUDA
 # the GPU routines will probably require to use inv and multiply the inpu by R^-1 to obtain Q (getri, getrf)
 
 function non_strided(a, b)
-    x = [@view a[:, :, i] for i in axes(a, 3)]
-    y = [@view b[:, :, i] for i in axes(b, 3)]
+    println("start view")
+    CUDA.@time x = [@view a[:, :, i] for i in axes(a, 3)]
+    CUDA.@time y = [@view b[:, :, i] for i in axes(b, 3)]
+    println("end view")
     CUDA.CUBLAS.gemm_batched('N', 'N', 1f0, x, y)
 end
 
